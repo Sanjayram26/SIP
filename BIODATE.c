84280fbdeed954e60019ba2a0e2a3d2eeab2f989
Sanjayram26 <Sanjaymoorthi35@gmail.com>
2024-09-19 15:48:21 +0530

    BIODATE.c

---
+++ BIODATE.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Student name             :S.sanjayram\n");
+    printf("Date of birth            :26.03.2007\n");
+    printf("Father name              :K.sathiyamoorthi\n");
+    printf("Mother name              :S.umamageshwari\n");
+    printf("sibling name             :S.dhushyanthram\n");
+    printf("Father occupation        :police(strking force)\n");
+    printf("Mother occupation        :nurce(senior staff)\n");
+    printf("Hobbies                  :listening songs,playing video games,involving physical activities\n");
+    printf("Describe about me        :Im an sports player.i got four tamilnadu state madel in fencing.i easily interact with everypersons and keep good and happy vibesss\n");
+    printf("Address                  :E24\4 police quarters,silluvampatty(po),namakkal(dt),637003\n");
+    printf("Nationality              :Indian\n");
+    printf("Aim                      :To become good government officer in this socioty \n");
+    
+    
+
+    return 0;
+}
